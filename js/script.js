ecf36bf15f0b20b291ed52cd1aee1386a5d7377a
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-04-26 14:43:01 +0000

    comments

--- js/script.js
+++ js/script.js
@@ -1,35 +1,19 @@
 // Copyright (c) 2022 Angelo Pintilie All rights reserved
+//
 // Created by: Angelo Pintilie
 // Created on: April 2022
 // This file contains the JS functions for index.html
 
-"use strict"
-
- // This function converts Celsius to Fahrenheit.
-function convertCelToFahr() {
-  // get user input
-  let height = parseFloat(document.getElementById('height').value)
-  let baseEdge = parseFloat(document.getElementById('base-edge').value)
-
-  // calculate the temperature in fahrenheit from celsius
-  let volume = (2(1+Math.sqrt(2))base-edge**2*height)
-  
-  // display the results
-  document.getElementById('volume').innerHTML = "the volume of the octagonal prism " + volume.toFixed(2) + "cm³"
-  
-}
-
-// This function converts Fahrenheit to Celsius.
-
-function convertFahrToCel() {
-  // get user input
-  let tempFahr = parseFloat(document.getElementById('fahr').value)
-
-
-  // calculate the temperature in celsius from fahrenheit
-  let tempCels = (tempFahr - 32.0) * (5/9) 
-  
-  // display the results
-  document.getElementById('tempCels').innerHTML = "The temperature in Celsius is " + tempCels.toFixed(2) + " °C"
-
+function algorithm () {
+  // variable for height
+  let height = parseFloat(document.getElementById("height").value)
+  // variable for base edge
+  let baseEdge = parseFloat(document.getElementById("base-edge").value)
+  // calculates surface area
+  const SURFACE_AREA = (8*base-edge*height+4(1+Math.sqrt(2)a**2)
+  // calculates the volume 
+  const VOLUME = (2(1+Math.sqrt(2))base-edge**2*height)
+  // prints out results
+  document.getElementById('surface-area').innerHTML = "surface area = " + SURFACE_AREA.toFixed(2)
+  document.getElementById('volume').innerHTML = "volume = " + VOLUME.toFixed(2)
 }
