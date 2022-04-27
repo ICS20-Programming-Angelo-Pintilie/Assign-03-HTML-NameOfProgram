f717036813eb9f08c8b9cb1fcfcd29fb3132f710
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-04-27 14:33:42 +0000

    it was literally an a that i didn't change into a variable :|

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@
 // Created by: Angelo Pintilie
 // Created on: April 2022
 // This file contains the JS functions for index.html
+'use strict'
 
-function algorithm () {
+function algorithm() {
   // variable for height
   let height = parseFloat(document.getElementById("height").value)
   // variable for base edge
   let baseEdge = parseFloat(document.getElementById("base-edge").value)
   // calculates surface area
-  let surfaceArea = (8*base-edge*height+4(1+Math.sqrt(2)a**2)
+  let surfaceArea = (8 * baseEdge * height + 4 * (1 + Math.sqrt(2)) * baseEdge ** 2)
   // calculates the volume 
-  let volume = (2(1+Math.sqrt(2))base-edge**2*height)
+  let volume = (2 * (1 + Math.sqrt(2)) * baseEdge ** 2 * height)
   // prints out results
   document.getElementById('surface-area').innerHTML = "surface area = " + surfaceArea.toFixed(2)
   document.getElementById('volume').innerHTML = "volume = " + volume.toFixed(2)
